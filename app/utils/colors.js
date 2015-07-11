5bae3f159ff30bfd6f2518f213c8134660c7a718
alexlopez <alehlopeh@gmail.com>
2015-07-11 17:41:59 -0400

    Use rgba for colors

--- app/utils/colors.js
+++ app/utils/colors.js
@@ -1,13 +1,25 @@
 const colors = {
-  rollingStone:       '#788187',
-  mediumPurple:       '#8459DA',
-  mediumPurple2:      '#8359DA',
-  white:              '#FFFFFF',
-  shuttleGray:        '#566067',
-  outerSpace:         '#2C3941',
-  sushi:              '#42B57D',
-  atlantis:           '#81D135',
-  mandy:              '#E84A6B'
+  
+  rollingStone:       'rgb(120,129,135)',
+
+  mediumPurple:       'rgb(132,89,218)',
+
+  mediumPurple20:     'rgba(132,89,218, 0.20)',
+
+  white:              'rgb(255,255,255)',
+
+  white20:            'rgba(255, 255, 255, 0.20)',
+
+  shuttleGray:        'rgb(86,96,103)',
+
+  outerSpace:         'rgb(44,57,65)',
+
+  sushi:              'rgb(66, 181, 125)',
+
+  atlantis:           'rgb(129, 209, 53)',
+
+  mandy:              'rgb(232, 74, 107)'
+
 }
 
 module.exports = colors;
