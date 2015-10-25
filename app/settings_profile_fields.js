ae8dd41030662700652968089e51c1365f0c92ca
alexlopez <alehlopeh@gmail.com>
2015-10-25 01:08:58 -0400

    add settings json file locally

---
+++ app/settings_profile_fields.js
@@ -0,0 +1,62 @@
+{
+    "firstname": {
+        "label": "First Name",
+        "field_type": "input",
+        "long_label": "What should we call you?",
+        "sub_label": "Fake names get 93.6% less matches."
+    },
+    "birthday": {
+        "label": "Birthday",
+        "field_type": "birthday"
+    },
+    "gender": {
+        "label": "Gender",
+        "field_type": "dropdown",
+        "values": ["Male","Female"]
+    },
+    "email": {
+        "label": "Email",
+        "field_type": "input",
+        "placeholder": "Add Email"
+    },
+    "phone": {
+        "label": "Phone Number",
+        "field_type": "phone_input",
+        "placeholder": "Phone Number"
+    },
+    "body_type": {
+        "label": "Body Type",
+        "field_type": "dropdown",
+        "values": ["Thin","Fit","Average","Jacked","Curvy","Full Figured","A Little Extra","Overweight"]
+    },
+    "height": {
+        "label": "Height",
+        "field_type": "dropdown",
+        "values": ["5","6","7","8","9","10","11"]
+    },
+    "eye_color": {
+        "label": "Eye Color",
+        "field_type": "dropdown",
+        "values": ["Blue","Brown","Black"]
+    },
+    "hair_color": {
+        "label": "Hair Color",
+        "field_type": "dropdown",
+        "values": ["Black","Blonde","Brunette","Gray","Multi","Red"]
+    },
+    "ethnicity": {
+        "label": "Ethnicity",
+        "field_type": "dropdown",
+        "values": ["White","Black","Asian","Other"]
+    },
+    "smoke": {
+        "label": "Smoke",
+        "field_type": "dropdown",
+        "values": ["Yes","No"]
+    },
+    "drink": {
+        "label": "Drink",
+        "field_type": "dropdown",
+        "values": ["Yes","No"]
+    }
+}
