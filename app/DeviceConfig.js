060fbe480e378d43211d65860f1228674f3e7d6f
alexlopez <alehlopeh@gmail.com>
2015-11-19 00:25:12 -0500

    stop treating iphone 6 as a small device for purposes of magic numbers

--- app/DeviceConfig.js
+++ app/DeviceConfig.js
@@ -4,21 +4,22 @@ import Device from 'react-native-device'
 
 const screen = Dimensions.get('window')
 // console.log(AppInfo,Device)
+console.log(screen)
 // const device = 'iphone6Plus'
 // window.AppInfo = AppInfo
 // console.log(AppInfo.getInfoDeviceName(),AppInfo.getInfoDisplayName(),AppInfo.getInfoName())
 const DeviceConfig = {
   MagicNumbers:{
     is4s: screen.height <= 480,
-    isSmallDevice: screen.scale < 3,
-    screenWidth: screen.scale == 3 ? (screen.width - 40) : (screen.width - 20),
-    screenPadding: screen.scale == 3 ? 40 : 20,
-    size18: screen.scale == 3 ? 18 : 16,
+    isSmallDevice: screen.width < 360,
+    screenWidth: screen.width > 360 ? (screen.width - 40) : (screen.width - 20),
+    screenPadding: screen.width > 360 ? 40 : 20,
+    size18: screen.width > 360 ? 18 : 16,
     continueButtonHeight: screen.scale == 2 && screen.height <= 480 ? 60 : 80
     // HeaderFontSize: Device.scale == 3 ? 20 : 20,
   }
 
 }
 
-console.log(screen)
+console.log(DeviceConfig)
 export default DeviceConfig
