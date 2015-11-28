4d33d480ef2575153b1b85814365ce2b0f3dcdf8
alexlopez <alehlopeh@gmail.com>
2015-11-28 13:45:24 -0500

    Adjust the padding globally (maybe undo this)

--- app/DeviceConfig.js
+++ app/DeviceConfig.js
@@ -24,8 +24,8 @@ const DeviceConfig = {
     keyboardHeight: keyboardHeightMapShort[screen.height],
     is4s: screen.height <= 480,
     isSmallDevice: screen.width < 360,
-    screenWidth: screen.width > 360 ? (screen.width - 40) : (screen.width - 20),
-    screenPadding: screen.width > 360 ? 40 : 20,
+    screenWidth: screen.width > 360 ? (screen.width - 50) : (screen.width - 25),
+    screenPadding: screen.width > 360 ? 50 : 25,
     size18: screen.width > 360 ? 18 : 16,
     continueButtonHeight: screen.scale == 2 && screen.height <= 480 ? 60 : 80
   }
