03d19ea4ea6ea22ebbeb12b83f5aedb0d163077b
alexlopez <alehlopeh@gmail.com>
2015-11-28 07:20:39 -0500

    adopt a package that gets device info, slightly modify

---
+++ lib/react-native-device/DeviceUtil.m
@@ -0,0 +1,60 @@
+#import "DeviceUtil.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+#import "RCTUtils.h"
+#import "RCTConvert.h"
+#import "sys/utsname.h"
+
+@implementation DeviceUtil
+
+RCT_EXPORT_MODULE();
+
+@synthesize bridge = _bridge;
+
+- (instancetype)init
+{
+  if (self = [super init]) {
+    UIDevice *device = [UIDevice currentDevice];
+  }
+  
+  return self;
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (NSDictionary *)constantsToExport {
+  UIDevice *device = [UIDevice currentDevice];
+  
+  return @{
+           @"model" : self.deviceName,
+           @"name" : (device.name),
+           @"systemName" : (device.systemName),
+           @"systemVersion" : (device.systemVersion),
+           @"localizedModel" : (device.localizedModel),
+           @"multitaskingSupported" : ([NSNumber numberWithBool:device.multitaskingSupported]),
+           @"userInterfaceIdiom" : self.userInterfaceIdiom,
+           @"identifierForVendor" : (device.identifierForVendor.UUIDString),
+           };
+}
+
+- (NSString *)userInterfaceIdiom {
+  switch ([[UIDevice currentDevice] userInterfaceIdiom]) {
+    case UIUserInterfaceIdiomPad: return @"Pad"; break;
+    case UIUserInterfaceIdiomPhone: return @"Phone"; break;
+    default: return @"Unspecified"; break;
+  }
+}
+
+- (NSString *)deviceName
+{
+  struct utsname systemInfo;
+  uname(&systemInfo);
+  
+  return [NSString stringWithCString:systemInfo.machine
+                            encoding:NSUTF8StringEncoding];
+}
+
+@end
