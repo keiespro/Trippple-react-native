fd9a520a9be48c91762a333bf6c94db493ebc3c1
alexlopez <alehlopeh@gmail.com>
2016-01-22 05:13:50 -0500

    disable promise exception unhandled reporting

--- index.ios.js
+++ index.ios.js
@@ -3,24 +3,26 @@
 * @flow
 */
 
-import React, { AppRegistry } from 'react-native'
+import React, { View,AppRegistry } from 'react-native'
 import Boot from './app/Boot'
 import Log from './app/utils/logger'
+import alt from './app/flux/alt'
 
-delete window.Promise
-import Promise from 'bluebird'
-window.Promise = Promise
+// delete window.Promise
+// import Promise from 'bluebird'
+// window.Promise = Promise
+  window.React = React;
+  window.alt = alt;
 
 if(__DEV__){
   console.ignoredYellowBox = ['jsSchedulingOverhead'];
-  window.React = React;
-  window.onunhandledrejection = e => console.warn('unhandled', e);
-  window.onrejectionhandled = e => console.warn('handled', e );
+  // window.onunhandledrejection = e => console.warn('unhandled', e);
+  // window.onrejectionhandled = e => console.warn('handled', e );
 }
 
-Promise.onPossiblyUnhandledRejection = ((error) => {
-   Log('Unhandled rejection',err);
-});
+// Promise.onPossiblyUnhandledRejection = ((error) => {
+//    Log('Unhandled rejection',err);
+// });
 
 class trippple extends React.Component{
   constructor(props){
@@ -28,7 +30,7 @@ class trippple extends React.Component{
   }
 
   render(){
-    return <Boot/>
+    return <View style={{backgroundColor:'red'}}><Boot/></View>
   }
 }
 
