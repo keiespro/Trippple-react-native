444722af8e4c49b246f6c3efeb07a38678f63f62
alexlopez <alehlopeh@gmail.com>
2016-01-26 12:27:11 -0500

    autoupdater big improvements

--- AutoUpdater/bin/deploy-ota.js
+++ AutoUpdater/bin/deploy-ota.js
@@ -1,144 +1,245 @@
-#! /usr/bin/env node
-import program from 'commander';
-import { process } from "core-worker";
+#! /usr/bin/env babel-node
 
-
+import program from 'commander-plus';
 import Promise from 'bluebird';
+import chalk from 'chalk';
+import jsonfile from 'jsonfile';
+import firebaseTool from 'firebase-tools';
+import fs from 'fs.extra';
+import {execSync} from 'child_process';
 
-var child;
-import {terminal} from 'terminal-kit';
-const term = terminal;
-
-import jsonfile from 'jsonfile'
-
-const T3 = new TripppleTools();
-
-const APP_VERSION = '2.0.17';
-
-program.version('0.0.1')
-
-var jsonfile = require('jsonfile')
-
-var util = require('util')
-
-var file = '/.versionfile.json'
-
-
-const VERSION =  jsonfile.readFileSync(file);
-console.dir(VERSION)
-
-const list = ['all', 'build', 'bump', 'deploy', 'exit'];
-term.bold.underline.red('Trippple\n');
-term.bold.underline.brightYellow('==============================================================\n');
-term.bold.underline.brightYellow('==============================================================\n');
-
-console.log('What do you want to do:');
-
-
-program.choose(list, function(i){
-
-  switch(list[i]){
-    case 'exit':
-      term.bold.underline.brightYellow('GOOD BYE\n');
-      return;
-      break;
-    case 'all':
-      T3.everything();
-      break;
-    case 'bump':
-      T3.bump();
-      break;
-    case 'build':
-      T3.build();
-      break;
-    case 'deploy':
-      T3.deploy();
-      break;
-  }
-
-  console.log('you chose %d "%s"', i, list[i]);
-});
-
+const NPM_CONFIG_PREFIX = process.env.npm_config_prefix || '';
+const BUILD_NUMBER = '1';
+const UPDATE_CHECK_FILE = 'AutoUpdater/public/update.json';
+const APP_VERSION = '2.0';
+const VERSION_FILE = '.versionfile.json';
+const ENV = process.env.NODE_ENV || 'development';
+const BASE_URLS = {
+  development: 'http://x.local:5000',
+  production: 'https://blistering-torch-607.firebaseapp.com'
+}
+// coloring
+const error = chalk.bold.red;
+const bg = chalk.bgMagenta;
+const success = chalk.green;
 
 
 class TripppleTools{
 
-  build(){
-    // const proc = process(`./node_modules/react-native/packager/packager.sh bundle --platform=ios --entry-file=index.ios.js --bundle-output=main.jsbundle --verbose --assets-dest=./assets/newimg --project-root=./ --reset-cache=true`)
-    // .then((error, stdout, stderr) {
-    //   term.green('stdout: ' + stdout);
-    //   term.red.error('stderr: ' + stderr);
-
-    // })
-    // .catch((error) =>{
-    //   if (error !== null) {
-    //     term.red.error('exec error: ' + error);
-    //   }
-    // })
+  constructor(){
+    this.VERSION =jsonfile.readFileSync(VERSION_FILE);
+    this.BUILD_NUMBER = BUILD_NUMBER;
   }
 
-  bump(){
-    term.red.error('bump version')
+  init(){
+    console.log(chalk.bgMagenta.bold('\nTrippple\n'));
+    this.presentChoice()
+  }
 
-    const file = 'AutoUpdater/public/update.json'
-    const filejson = {
-      "version": APP_VERSION,
-      "minContainerVersion": "0",
-      "url": {
-        "url": "https://blistering-torch-607.firebaseapp.com/main.jsbundle",
-        "isRelative": false
+  getOutputFilename(){ return `main-${this.VERSION}-${BUILD_NUMBER}-${this.timestamp}.jsbundle`; }
+
+  getOutputPath(){ return `AutoUpdater/public/${this.getOutputFilename()}` }
+
+  getEnvironmentBaseUrl(){
+    return BASE_URLS[ENV]
+  }
+
+  getBackupPath(){ return 'bundles/' }
+
+  dev(){
+    console.log(chalk.bgMagenta.yellow('\nServing update.json and compiled bundle\n'));
+    firebaseTool.serve({host:'x.local'})
+  }
+
+  presentChoice(){
+
+    let choiceList = ['dev', 'dist', 'compile', 'bump', 'deploy', 'rollback', 'exit'];
+
+
+    program.choose(choiceList, (i) => {
+
+      console.log(chalk.bgMagenta.bold('\n'+choiceList[i].toUpperCase()+'\n'));
+
+      this.timestamp = Date.now()
+
+      if(choiceList[i] == 'exit'){
+        console.log(chalk.yellow('GOOD BYE\n'));
+        process.exit()
+      }else{
+        var choice = this[choiceList[i]];
+        choice();
       }
+
+    })
+  }
+
+  compile(opts={}) {
+    console.log(bg('Compiling jsbundle'))
+
+    return new Promise((resolve, reject)=> {
+      const flags = `bundle --platform=ios --entry-file=index.ios.js --bundle-output=${this.getOutputFilename()} --verbose --assets-dest=./assets/ --project-root=./ --reset-cache=true`;
+
+      const child = exec(`${NPM_CONFIG_PREFIX}/bin/react-native ${flags}`)
+
+      child.stdout.on('data', (data) => console.log(chalk.cyan('|> ') + data));
+
+      child.stderr.on('data', (data) => console.log(chalk.red('|> ') + data));
+
+      child.on('close', (code) => {
+        console.error(error(`child process exited with code ${code}`));
+        if(!opts.dist){
+          return resolve(this.cleanUp())
+        }else{
+          resolve()
+        }
+      });
+
+      child.on('error', (err) => {
+        console.log(error('Failed to start child process.'));
+        return reject(this.cleanUp())
+      });
+
+    })
+  }
+
+  bump(opts={}){
+    return new Promise((resolve, reject) => {
+
+      console.log(bg('Bump JS version and regenerate update.json'))
+      let {app} = this.versionInfo;
+      let V = this.VERSION;
+      let pieces =  V.split('.')
+
+      let bumped = parseInt(pieces.pop()) * (opts.rollback ? -1 : 1)
+      let result = [...pieces,bumped].join('.')
+      console.log(result);
+
+      /* UPDATE.JSON TEMPLATE ----------------------------------------------------  */
+
+      let updateCheckPayload = {
+        "version": result,
+        "minContainerVersion": app,
+        "url": {
+          "url": `${this.getEnvironmentBaseUrl()}/${this.getOutputFilename()}`,
+          "isRelative": false
+        }
+      };
+
+      /* END UPDATE.JSON TEMPLATE ------------------------------------------------- */
+
+      console.log(`\n${success(this.getOutputFilename())} \n${updateCheckPayload} \n`)
+
+      jsonfile.writeFileSync(UPDATE_CHECK_FILE, updateCheckPayload, {spaces: 2}, (err) => {
+        console.error(error('1'))
+        console.error(error(err))
+      });
+
+      jsonfile.writeFileSync(VERSION_FILE, result, {spaces: 2}, (err) => {
+        console.error(error('2'))
+        console.error(error(err))
+      });
+
+      this.VERSION = result;
+
+      console.log(success(`|> NOW ON VERSION ${this.VERSION}`))
+
+      if(!opts.dist){
+        resolve(this.cleanUp())
+      }else{
+        resolve(true)
+      }
+
+    })
+  }
+
+  deploy(opts={}){
+
+    console.log(bg(`Deploy time! (${ENV})`));
+
+    if(ENV == 'production'){
+      console.log(error(`firebase deploy  ${this.VERSION} ${this.timestamp}`));
+
+      try {
+
+        firebaseTool.deploy.hosting()
+
+      } catch(err) {
+        console.log(error(err))
+        throw err
+      }
+
+    }else{
+      console.log(error(`local deploy ${this.VERSION} ${timestamp}`));
     }
 
-    jsonfile.writeFileSync(file, filejson, {spaces: 2});
-
-    return true;
-  }
-
-  deploy(){
-    const timenow = new Date()
-
-    term.bold.underline.red(`cd AutoUpdater && firebase deploy:hosting -m ${APP_VERSION} ${timenow}`);
-
-    try {
-
-      const proc = await process(`cd AutoUpdater && firebase deploy:hosting -m ${APP_VERSION} ${timenow}`)
-
-      // work with result
-    } catch(error) {
-      if (error !== null) {
-        term.bold.underline.brightYellow('==============================================================\n');
-        term.bold.underline.red('exec error: ' + error);
-        term.bold.underline.brightYellow('==============================================================\n');
-
+    // Copy bundle to local firebase public dir
+    fs.copy(this.getOutputFilename(), this.getOutputPath(), { replace: false }, (err) => {
+      if (err) { // i.e. file already exists or can't write to directory
+        console.error(error(err));
+        throw err;
       }
-    }
 
+      console.log(success(`Copied ${this.getOutputFilename()} to ${this.getOutputPath()}`));
+
+      // Move bundle extra bundle to ./bundles/
+      fs.move(this.getOutputFilename(), this.getBackupPath()+this.getOutputFilename(), (err) => {
+        if (err) { // i.e. file already exists or can't write to directory
+          console.error(error(err));
+          throw err;
+        }
+        console.log(success(`Copied ${this.getOutputPath()} to ${this.getBackupPath()+this.getOutputFilename()}`));
+
+        if(!opts.dist){
+          this.cleanUp()
+        }else{
+          console.log(success('DIST BUILD COMPLETE!'));
+          this.cleanUp()
+          return
+        }
+
+      })
+    })
   }
 
-  everything(){
+  rollback(){
+    this.compile({rollback:true})
+  }
 
-    this.build();
-    this.bump();
-    this.deploy();
+  dist(){
 
+    program.confirm('Compile -> Bump -> Deploy. Do all 3?', (ok) => {
+      if(ok){
+        var run = {dist:true}
+        this.compile(run).then((r)=> this.bump(run)).then((r2) => this.deploy(run) ).catch((err)=>{
+          console.error(error(err));
+        })
+      }
+    });
+  }
+
+  cleanUp(){
+    this.timestamp = Date.now();
+    this.presentChoice()
   }
 }
 
+/*** do it ***/
+const T3 = new TripppleTools();
 
+{
+  T3.init();
 
-
-program
+  program
   .command('*')
-  .on('--help', function(){
-  term.bold.underline.red('Deploy OTA update to Trippple client.');
-  term.green('  Examples:');
-  console.log('');
-  console.log('    $ npm run deploy-ota dammit');
-  console.log('    $ ./bin/deploy-ota.js dammit');
-  console.log('');
-});
+  .on('--help', () => {
+    console.log('Deploy OTA update to Trippple client.');
+    console.log('Examples:');
+    console.log('');
+    console.log('    $ npm run autoupdate');
+    console.log('    $ ./bin/deploy-ota.js dammit');
+    console.log('');
+  });
 
-
-program.parse(process.argv);
-
+  program.version(APP_VERSION)
+  program.parse(process.argv);
+}
