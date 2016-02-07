2b16794d836a2944755bd865fea964d8b90bc13f
alexlopez <alehlopeh@gmail.com>
2016-02-07 03:49:11 -0500

    better app delegate google tracking id update minimum version for js

--- ios/Trippple/AppDelegate.m
+++ ios/Trippple/AppDelegate.m
@@ -24,22 +24,27 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 
+  NSString *env = [[NSProcessInfo processInfo] environment][@"ENV"];
+  NSLog(@"RUNNING IN %@",env);
+  
+  NSURL* defaultMetadataFileLocation = [[NSBundle mainBundle] URLForResource:@"metadata"
+                                                               withExtension:@"json"];
+  NSURL* latestJSCodeLocation;
   NSURL *defaultJSCodeLocation;
+  NSString *rootUrl;
+
+  if([env isEqual: @"production"]){
+    // PRODUCTION
+    defaultJSCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    rootUrl = @"https://blistering-torch-607.firebaseapp.com/";
+  }else{
+    // DEVELOPMENT
+    defaultJSCodeLocation = [NSURL URLWithString:@"http://x.local:8081/index.ios.bundle?platform=ios&dev=true"];
+    rootUrl = @"http://localhost/"; // AutoUpdater won't accept a rootUrl with a port
+  }
   
   ReactNativeAutoUpdater* updater = [ReactNativeAutoUpdater sharedInstance];
   [updater setDelegate:self];
-  
-  // PRODUCTION
-  defaultJSCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-  NSString *rootUrl = @"https://blistering-torch-607.firebaseapp.com/";
-  
-  // DEVELOPMENT
-//    defaultJSCodeLocation = [NSURL URLWithString:@"http://x.local:8081/index.ios.bundle?platform=ios&dev=true"];
-//    NSString *rootUrl = @"http://localhost/"; // AutoUpdater won't accept a rootUrl with a port
-  
-  
-  
-  NSURL* defaultMetadataFileLocation = [[NSBundle mainBundle] URLForResource:@"metadata" withExtension:@"json"];
   [updater initializeWithUpdateMetadataUrl:[NSURL URLWithString:JS_CODE_METADATA_URL]
                      defaultJSCodeLocation:defaultJSCodeLocation
                defaultMetadataFileLocation:defaultMetadataFileLocation ];
@@ -48,15 +53,24 @@
   [updater downloadUpdatesForType: ReactNativeAutoUpdaterPatchUpdate];
   [updater checkUpdate];
 
-  NSURL* latestJSCodeLocation =  [updater latestJSCodeLocation]; //PRODUCTION
-//  NSURL* latestJSCodeLocation = defaultJSCodeLocartion; // DEVELOPMENT
+  if([env  isEqual: @"production"]){
+    //PRODUCTION
+    latestJSCodeLocation = [updater latestJSCodeLocation];
+  }else{
+    // DEVELOPMENT
+    latestJSCodeLocation = defaultJSCodeLocation;
+  }
   
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   self.window.rootViewController = rootViewController;
-  RCTBridge* bridge = [[RCTBridge alloc] initWithBundleURL:latestJSCodeLocation moduleProvider:nil launchOptions:nil];
+  RCTBridge* bridge = [[RCTBridge alloc] initWithBundleURL:latestJSCodeLocation
+                                            moduleProvider:nil
+                                             launchOptions:nil];
   
-  RCTRootView* rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"trippple" initialProperties:nil];
+  RCTRootView* rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"trippple"
+                                            initialProperties:nil];
   
   rootView.backgroundColor = [UIColor tr_outerSpaceColor];
   self.window.rootViewController.view = rootView;
@@ -75,16 +89,13 @@
     RCTRootView* rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                      moduleName:@"trippple"
                                               initialProperties:nil];
-    
-    
+    rootView.backgroundColor = [UIColor tr_outerSpaceColor];
+
     self.window.rootViewController.view = rootView;
     
   });
 }
 
-
-// RN >= 0.18.0 SYNTAX //
-//
 // Required to register for notifications
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
 {
