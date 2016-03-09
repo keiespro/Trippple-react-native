d4ea98f4e0f7296be017ec0e7f3cbcf0539d7302
alexlopez <alehlopeh@gmail.com>
2016-03-08 21:26:43 -0500

    disable local notificaiton at midnight

--- app/components/main.js
+++ app/components/main.js
@@ -52,7 +52,7 @@ class Main extends Component{
     this.refs.nav.navigationContext.addListener('didfocus', (e)=>{
       AppActions.updateRoute.defer(this.refs.nav.state.presentedIndex)
     })
-    NotificationActions.scheduleNewPotentialsAlert.defer()
+    // NotificationActions.scheduleNewPotentialsAlert.defer()
       // AppActions.sendTelemetry(this.props.user)
   }
 
