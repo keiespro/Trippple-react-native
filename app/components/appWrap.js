2f9111259cc06a4a8cf06d866083a343d78d43b2
alexlopez <alehlopeh@gmail.com>
2017-02-03 00:08:46 -0500

    commit new appwrapper

---
+++ app/components/appWrap.js
@@ -0,0 +1,37 @@
+import { StatusBar, View, Dimensions, NativeModules, Text, Platform, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { connect } from 'react-redux';
+
+import App from './app'
+
+
+class AppWrap extends React.Component{
+
+  state = {};
+    componentDidMount(){
+      if(this.props.navigation.navigators){
+        this.setState({ready:true})
+
+      }
+    }
+  componentWillReceiveProps(nProps){
+
+    if(nProps.navigation.navigators && !this.props.navigation.navigators){
+      this.setState({ready:true})
+    }
+  }
+  render(){
+    return this.state.ready ? <App/> : null
+  }
+}
+const mapStateToProps = ({user, ui, navigation}, ownProps) => {
+  return {...ownProps,  navigation }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppWrap);
