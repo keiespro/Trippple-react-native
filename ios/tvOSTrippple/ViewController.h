116c81689a4556e7cb2e554c79950615de6b3a60
alexlopez <alehlopeh@gmail.com>
2017-02-08 04:37:13 -0500

    show purple dot in chat icon if there are new matches

---
+++ ios/tvOSTrippple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tvOSTrippple
+//
+//  Created by Alex Lopez on 2/8/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
