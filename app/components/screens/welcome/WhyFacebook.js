d060029c15d730f1e1fc03f898feb60c617362c9
Javier Cubides <javiercferrans@gmail.com>
2017-04-12 18:08:15 -0500

    Updated language in why facebook modal and refactored it to a stateless component

--- app/components/screens/welcome/WhyFacebook.js
+++ app/components/screens/welcome/WhyFacebook.js
@@ -1,45 +1,34 @@
 import { StyleSheet, Text, Dimensions, View, TouchableOpacity, ScrollView, Image } from 'react-native';
-import React, { Component } from 'react';
-import {BlurView} from 'react-native-blur'
-
+import React, {PropTypes} from 'react';
 import colors from '../../../utils/colors'
 import { MagicNumbers } from '../../../utils/DeviceConfig'
+
 const DeviceHeight = Dimensions.get('window').height;
 const DeviceWidth = Dimensions.get('window').width;
 
-class WhyFacebook extends Component {
+const WhyFacebook = ({ kill }) =>
+  <ScrollView style={{ width: DeviceWidth, height: DeviceHeight, backgroundColor: colors.outerSpace, padding: MagicNumbers.screenPadding}}>
+    <View style={{flexDirection: 'column', justifyContent: 'space-around', height: DeviceHeight - 50, flex: 1}}>
+      <Text style={[styles.allText, styles.titleText, {textAlign: 'center', marginTop: 20}]}>WHY FACEBOOK?</Text>
+      <View>
+        <Text style={[styles.allText, styles.titleText, {}]}>HIDE FROM YOUR FRIENDS</Text>
+        <Text style={[styles.allText, {}]}>Facebook login makes it easier to hide you from Facebook friends and family.</Text>
+      </View>
+      <View>
+        <Text style={[styles.allText, styles.titleText, {}]}>NO FAKE USERS</Text>
+        <Text style={[styles.allText, {}]}>Facebook does a pretty good job at purging fake users. Signing up with Facebook lowers the chances of fake users joining Trippple.</Text>
+      </View>
+      <View>
+        <Text style={[styles.allText, {}]}>Note: We do not store inactive user information. Disabling your Trippple account will delete all of your Facebook information from Trippple.</Text>
+      </View>
+      <TouchableOpacity style={{backgroundColor: colors.shuttleGray, padding: 10, alignSelf: 'center', borderRadius: 50}} onPress={() => { kill() }}>
+        <Image source={require('./assets/close@3x.png')} style={{width: 15, height: 15}} />
+      </TouchableOpacity>
+    </View>
+  </ScrollView>
 
-  static propTypes = {
-    style: View.propTypes.style,
-    onPress: React.PropTypes.func,
-    onLogin: React.PropTypes.func,
-    onLogout: React.PropTypes.func,
-  };
-
-  constructor(props) {
-    super()
-  }
-
-  render() {
-    return (
-      <ScrollView style={{ width: DeviceWidth, height: DeviceHeight, backgroundColor: colors.outerSpace, padding: MagicNumbers.screenPadding}}>
-        <View style={{flexDirection: 'column', justifyContent: 'space-around', height: DeviceHeight - 50, flex: 1}}>
-          <Text style={[styles.allText, styles.titleText, {textAlign: 'center', marginTop: 20}]}>WHY FACEBOOK?</Text>
-          <View>
-            <Text style={[styles.allText, styles.titleText, {}]}>USER PRIVACY</Text>
-            <Text style={[styles.allText, {}]}>By signing up with Facebook, we can block your profile from your Facebook friends.</Text>
-          </View>
-          <View>
-            <Text style={[styles.allText, styles.titleText, {}]}>NO FAKE USERS</Text>
-            <Text style={[styles.allText, {}]}>Facebook does a pretty good job at purging fake users. Signing up with Facebook lowers the chances of fake users joining Trippple.</Text>
-          </View>
-          <TouchableOpacity style={{backgroundColor: colors.shuttleGray, padding: 10, alignSelf: 'center', borderRadius: 50}} onPress={() => { this.props.kill() }}>
-            <Image source={require('./assets/close@3x.png')} style={{width: 15, height: 15}}/>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
-  }
+WhyFacebook.propTypes = {
+  kill: PropTypes.func.isRequired
 }
 
 const styles = StyleSheet.create({
@@ -73,8 +62,8 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 16,
-    fontFamily: 'montserrat', fontWeight: '800',
-
+    fontFamily: 'montserrat',
+    fontWeight: '800',
   },
   iconButtonCouples: {
     borderColor: colors.mediumPurple,
