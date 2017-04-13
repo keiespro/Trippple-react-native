be0094cff63837d20d08a8b25e689d8c43f3ec21
alexlopez <alehlopeh@gmail.com>
2017-04-12 20:25:38 -0400

    simplify browse reducers

--- app/reducers/browseReducer.js
+++ app/reducers/browseReducer.js
@@ -1,9 +1,37 @@
-import {
-  combineReducers
-} from 'redux-immutable';
+import { combineReducers } from 'redux-immutable';
+import Immutable from 'immutable'
 
-import nearby from './browse/nearby'
-import newest from './browse/newest'
-import popular from './browse/popular'
+const initialState = new Immutable.OrderedMap()
 
-export default combineReducers({newest,nearby,popular})
+function createBrowseReducer(filter = 'newest') {
+
+  return function reducer(state = initialState, action) {
+
+    switch (action.type) {
+      case 'ONBOARD_FULFILLED':
+      case 'VERIFY_COUPLE_PIN_FULFILLED':
+      case 'HANDLE_NOTIFICATION_COUPLE_READY':
+      case 'HANDLE_NOTIFICATION_DECOUPLE':
+      case 'CLEAR_POTENTIALS':
+        return initialState;
+
+      case 'SEND_LIKE_FULFILLED':
+        return action.meta.filter == filter ? state.merge({
+          user: {
+            liked: action.meta.likeStatus == 'approve',
+          },
+          likedAt: Date.now()
+        }) : state
+
+      case 'FETCH_BROWSE_FULFILLED':
+        if(action.meta.filter != filter) return state;
+        return action.payload ? state.merge(action.payload) : state;
+
+      default:
+        return state;
+    }
+  }
+
+}
+
+export default combineReducers(['newest', 'nearby', 'popular'].map(createBrowseReducer))
