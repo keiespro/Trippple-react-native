1dee06bef4838a58ac610ef6f656948e89cd44d3
Hiroki Moto <hiroki.moto.pro@gmail.com>
2017-06-14 17:57:54 +0900

    Fix facebook privacy modal

--- app/components/screens/welcome/WhyFacebook.js
+++ app/components/screens/welcome/WhyFacebook.js
@@ -1,5 +1,13 @@
-import { StyleSheet, Text, Dimensions, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import React from 'react';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
 import colors from '../../../utils/colors'
 import { MagicNumbers } from '../../../utils/DeviceConfig'
@@ -9,18 +17,26 @@ const DeviceWidth = Dimensions.get('window').width;
 
 const WhyFacebook = ({kill}) => (
   <ScrollView style={{ width: DeviceWidth, height: DeviceHeight, backgroundColor: colors.outerSpace, padding: MagicNumbers.screenPadding}}>
-    <View style={{flexDirection: 'column', justifyContent: 'space-around', height: DeviceHeight - 50, flex: 1}}>
+    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-around', height: DeviceHeight - 50}}>
       <Text style={[styles.allText, styles.titleText, {textAlign: 'center', marginTop: 20}]}>WHY FACEBOOK?</Text>
       <View>
-        <Text style={[styles.allText, styles.titleText, {}]}>USER PRIVACY</Text>
-        <Text style={[styles.allText, {}]}>By signing up with Facebook, we can block your profile from your Facebook friends.</Text>
+        <Text style={[styles.allText, styles.titleText, {}]}>HIDE FROM YOUR FRIENDS</Text>
+        <Text style={[styles.allText, {}]}>Facebook login makes it easier to hide you from Facebook friends and family</Text>
       </View>
       <View>
         <Text style={[styles.allText, styles.titleText, {}]}>NO FAKE USERS</Text>
         <Text style={[styles.allText, {}]}>Facebook does a pretty good job at purging fake users. Signing up with Facebook lowers the chances of fake users joining Trippple.</Text>
       </View>
-      <TouchableOpacity style={{backgroundColor: colors.shuttleGray, padding: 10, alignSelf: 'center', borderRadius: 50}} onPress={() => { kill() }}>
-        <Image source={require('./assets/close.png')} style={{width: 15, height: 15}} />
+      <View>
+        <Text style={[styles.allText, {color: colors.steelGrey}]}>
+          Note: We do not store inactive user information. Disabling your Tripple account will delete all of your Facebook information from Trippple.
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={{backgroundColor: colors.transparent, alignSelf: 'center'}}
+        onPress={() => { kill() }}
+      >
+        <Image source={require('../../../assets/close.png')} style={{width: 35, height: 35}} />
       </TouchableOpacity>
     </View>
   </ScrollView>
