f44995d5ae3a400f088c67cae9e2ee61a69f13b2
Hiroki Moto <hiroki.moto.pro@gmail.com>
2017-06-24 17:04:04 +0900

    Improve code quality

--- index.android.js
+++ index.android.js
@@ -1,33 +1,32 @@
 import React from "react";
 import ReactNative, { AppRegistry } from "react-native";
-import NewBoot from './app/NewBoot'
+import NewBoot from './app/NewBoot';
 
-
-if(typeof window !== 'undefined' && (__DEV__ ) && process.env.NODE_ENV !== 'production'){
-  global = window;
-  window.ReactNative = ReactNative;
-  window.React = React;
+if (typeof window !== 'undefined' && (__DEV__ ) && process.env.NODE_ENV !== 'production') {
+    global = window;
+    window.ReactNative = ReactNative;
+    window.React = React;
 }
 if(__DEV__){
-  console.log('index.ANDROID.js');
+    console.log('index.ANDROID.js');
 
-  console.ignoredYellowBox = [
-    `Possible Unhandled Promise Rejection`,
-    `{"line":`,
-    'jsSchedulingOverhead',
-    'SocketRocket',
-    'ScrollView',
-    'WARNING',
-    'Value did not change',
-    'Value is a function',
-    '%cfont-weight',
-    'Warning',
-    'Task oprhaned'
-  ];
+    console.ignoredYellowBox = [
+        `Possible Unhandled Promise Rejection`,
+        `{"line":`,
+        'jsSchedulingOverhead',
+        'SocketRocket',
+        'ScrollView',
+        'WARNING',
+        'Value did not change',
+        'Value is a function',
+        '%cfont-weight',
+        'Warning',
+        'Task oprhaned'
+    ];
 }
 
 const Trippple = (props => <NewBoot {...props}/>)
 
 AppRegistry.registerComponent('Trippple', () => Trippple)
 
-export default Trippple
+export default Trippple;
--- index.ios.js
+++ index.ios.js
@@ -3,33 +3,33 @@ import ReactNative, { AppRegistry } from 'react-native';
 import NewBoot from './app/NewBoot';
 
 if (typeof window !== 'undefined' && (__DEV__) && process.env.NODE_ENV !== 'production') {
-  console.log('index.ios.js');
-  global = window;
-  window.ReactNative = ReactNative;
-  window.React = React;
+    console.log('index.ios.js');
+    global = window;
+    window.ReactNative = ReactNative;
+    window.React = React;
 }
 if (__DEV__) {
-  console.ignoredYellowBox = [
-    '{"line":',
-    'jsSchedulingOverhead',
-    'SocketRocket',
-    'Possible Unhandled Promise Rejection',
-    'ScrollView',
-    'WARNING',
-    'Value did not change',
-    'Value is a function',
-    '%cfont-weight',
-    'Warning',
-    'Task orphaned',
-    'Animated: `use',
-    'Image source',
-    `View #`,
-    `console.error`,
-  ];
-  console.ignoredRedBox = [
-    'Image source',
-    `console.error`,
-  ];
+    console.ignoredYellowBox = [
+        '{"line":',
+        'jsSchedulingOverhead',
+        'SocketRocket',
+        'Possible Unhandled Promise Rejection',
+        'ScrollView',
+        'WARNING',
+        'Value did not change',
+        'Value is a function',
+        '%cfont-weight',
+        'Warning',
+        'Task orphaned',
+        'Animated: `use',
+        'Image source',
+        `View #`,
+        `console.error`,
+    ];
+    console.ignoredRedBox = [
+        'Image source',
+        `console.error`,
+    ];
 }
 
 const Trippple = (props) => (<NewBoot {...props}/>);
